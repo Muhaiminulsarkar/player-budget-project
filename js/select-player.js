e87a688cd5656200f88884b1d4ea618b69e884ad
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2022-08-23 02:16:40 +0600

    javascript file added

---
+++ js/select-player.js
@@ -0,0 +1,44 @@
+const nameArray = [];
+
+function display(name) {
+
+    console.log(name);
+    const playerNames = document.getElementById('player-names');
+    playerNames.innerHTML = '';
+
+
+
+    for (let i = 0; i < name.length; i++) {
+        const namePlayer = (nameArray[i]);
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <td><span>${i + 1} - </span> ${namePlayer}</td>`
+        playerNames.appendChild(tr);
+    }
+
+}
+
+
+
+function addName(element) {
+    const playerName = element.parentNode.children[0].innerText;
+
+    // const playerObj = {
+    //     playerName: playerName,
+    // }
+
+    if (nameArray.length < 5) {
+        nameArray.push(playerName);
+    }
+    else {
+        alert('Dont be added more than 5 player')
+    }
+
+
+    // console.log(nameArray);
+
+
+
+    document.getElementById('total-name-add').innerText = nameArray.length;
+    display(nameArray);
+}
