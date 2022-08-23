515b38b6119e1522a124a19cab2aeb5bfc89afce
Muhaiminulsarkar <sarkarmuhaiminul@gmail.com>
2022-08-23 16:11:01 +0600

    player budget work added

--- js/select-player.js
+++ js/select-player.js
@@ -6,8 +6,6 @@ function display(name) {
     const playerNames = document.getElementById('player-names');
     playerNames.innerHTML = '';
 
-
-
     for (let i = 0; i < name.length; i++) {
         const namePlayer = (nameArray[i]);
         const tr = document.createElement('tr');
@@ -15,17 +13,10 @@ function display(name) {
         <td><span>${i + 1} - </span> ${namePlayer}</td>`
         playerNames.appendChild(tr);
     }
-
 }
-
-
 
 function addName(element) {
     const playerName = element.parentNode.children[0].innerText;
-
-    // const playerObj = {
-    //     playerName: playerName,
-    // }
 
     /* button disable after click */
     element.disabled = true;
@@ -39,11 +30,40 @@ function addName(element) {
         alert('Dont be added more than 5 player')
     }
 
-
-    // console.log(nameArray);
-
-
-
     document.getElementById('total-name-add').innerText = nameArray.length;
     display(nameArray);
 }
+
+// common function
+function getInputFieldValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    return inputFieldValue;
+}
+
+function getTextElementValueById(elementId) {
+    const textElement = document.getElementById(elementId);
+    const textElementValueString = textElement.innerText;
+    const textElementValue = parseFloat(textElementValueString);
+    return textElementValue;
+}
+
+document.getElementById('btn-player').addEventListener('click', function () {
+    const playerBudget = getInputFieldValueById('player-budget');
+    console.log(playerBudget);
+
+    let nameAddedNumber = getTextElementValueById('total-name-add');
+    console.log(nameAddedNumber);
+
+    const playerExpenses = playerBudget * nameAddedNumber;
+    console.log(playerExpenses);
+
+    const playerExpensesField = document.getElementById('player-expenses');
+    const playerExpensesStringValue = playerExpensesField.innerText;
+    const playerExpensesValue = parseFloat(playerExpensesStringValue);
+
+    playerExpensesField.innerText = playerExpenses; //player expense update
+
+
+})
